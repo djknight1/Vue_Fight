9bace46f3148f8d2c82550276454b4a72f1b1d0e
djknight1 <757389+djknight1@users.noreply.github.com>
2018-03-26 01:05:02 +0800

    Changesomething

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 ;(function () {
     'use strict';/*使用js的严格模式 让语言更有规范 报错更容易找到*/
+    var a=0;
+    /*组件之间的通信可以通过事件监听器来表示*/
+        var Event = new Vue();
 
+    Vue.component('task',{  /*Vue.component注册的就是子组件,在html文件内使用的标签就是父组件！！*/
+        template:'#task-tpl',   /*template里的是子组件的模板,整个就相当于子组件*/
+        props:["todo"],         /*子组件接受父组件的参数*/
+        methods:{               /*子组件的方法 子组件不能直接更改数据,要上传给父组件*/
+            action:function (name,param) {
+                Event.$emit(name,param);   /*子组件点击方法触发action方法,传递两个参数,一个是自定义事件名,另一个是参数*/
+            } ,                         /*action方法触发自定义名字为name的事件*/
+                                        /*在父组件挂载的时候触发 父组件挂载时就是Vue mounted*/
+        }
+    });
+
+
+
+
+
+
+    function copy(obj) {   /*assign用的太多了 把它封装起来*/
+        return Object.assign({},obj);
+    }
 
     new Vue({
         el: "#main",
@@ -8,13 +30,55 @@
             list: [], /*这是一个待办事项里面的数据*/
             current_content:{},   /*里面是搞事情那一栏的具体参数,如是否完成,具体内容,什么时候提醒*/
         },
+        mounted:function () {
+            var me = this;/*在下面this是event本身 不是vue*/
+            this.list=ms.get('list')||this.list; /*每次VUE初始化的时候 要把他取出来,当this.list是空的时候等于空*/
+            me.$on('remove',function (id) {
+               this.remove(id);
+            });
+            me.$on('change_completed',function (id) {
+                this.merge();
+            });
+            me.$on('set_current',function (params) {
+               this.set_current(params);
+            })
+        },
+        watch:{   /*当某个值发生变化时,添加事件,跟监听器差不多*/
+            list: {
+                deep:true,  /*不管这个对象有多深,当里面发生一个微小的变化的时候,都要执行下面的方法*/
+                handler :function (new_val,old_val) { /*变化之后的值和变化之前的值*/
+                    if(new_val){    /*代表增加和更新和删除*/
+                        ms.set('list',new_val);
+                    }
+                    else{           /*删光的时候*/
+                        ms.set('list',[]);
+                    }
+                }
+            }
+        },
         methods:{
             merge:function () {        /*回车敲下去后,能过增加一件事*/
-                var title = this.current_content.title;
-                if(!title&&title!==0) return; /*空的时候不接受,0的时候接受*/
-                var todo = Object.assign({}, this.current_content);
+                var is_update = this.current_content.id; /*current一直只有一个*/
+                /*input绑定的this.current-content.title是时时的
+                * 你在input框内输入的时候this.current已经增加了一个title
+                * 但是还没有id id是在你提交表单的时候调用方法才赋予的
+                * 所以可以通过下面的方法判断是更新还是submit
+                * 更新就代表已经有了id submit表示还没有id*/
+                if (is_update){
+                    var index=this.find_index_by_id(is_update);         /*返回满足item.id=is_update的第一个元素的位置*/
+                    Vue.set(this.list,index,copy(this.current_content));/*  this.list[index] = copy(this.current_content)在Vue里头数组的修改不能用这个*/
+                }
+                else {
+                    var title = this.current_content.title;
+                    if(!title&&title!==0) return; /*空的时候不接受,0的时候接受*/
+                    var todo =copy(this.current_content);
+                    todo.id = this.next_id();
+                    this.list.push(todo);
+                    a++;
+                }
+
+                this.reset_current();
                 console.log(this.list);
-                this.list.push(todo);
                 /*
                 *assign方法把后面的可枚举属性复制到第一个参数中,遇到重名的会实行替换
                 * 如object.assign({a:1},{a:2,b:2})结果是{a:2,b:2}
@@ -36,8 +100,33 @@
 
             },
             remove :function (id) {
-                this.list.splice(id,1); /*splice从数组中删除 第一个参数表示索引,第二个参数表示从索引开始删除的数目*/
+                var index = this.find_index_by_id(id); /*splice从数组中删除 第一个参数表示索引,第二个参数表示从索引开始删除的数目*/
+                this.list.splice(index,1);
             },
+
+            next_id :function () {
+                return a;
+            },
+
+            set_current:function (todo) {
+               this.current_content = copy(todo);/*这样的话你改变this.current_content todo就不会跟着改变*/
+
+            },
+            reset_current :function () {
+                this.set_current({});
+            },
+
+            find_index_by_id :function (id) {
+                return this.list.findIndex(function (item) {    /*返回满足item.id=is_update的第一个元素的位置*/
+                    return item.id == id;                       /*通过id找索引*/
+                });
+            },
+
+            change_completed:function (id) {
+                var index = this.find_index_by_id(id);
+                Vue.set(this.list[index],'completed',!this.list[index].completed);   /*completed刚开始没有默认为NULL*/
+                console.log(this.list)
+            }
         }
     });
     /*只有单页的内容 只需要new 一个vue就行了*/
